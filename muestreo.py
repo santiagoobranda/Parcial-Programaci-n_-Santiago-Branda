a6d43934d5d203cbeba56830022d99c695fd736c
santiagoobranda <santiago.branda14@gmail.com>
2026-05-21 16:32:59 -0300

    Modularizar el programa

---
+++ muestreo.py
@@ -0,0 +1,36 @@
+def mostrar_heroe(heroe):
+    """
+    Brief:
+        Muestra en la consola los datos formateados de un heroe accediendo a los indices de su lista.
+    Parametros:
+        heroe: Sublista con los datos del personaje posicionados por indice (0:Nombre, 1:Identidad, 
+                      2:Empresa, 3:Altura, 4:Peso, 5:Genero, 6:Ojos, 7:Pelo, 8:Fuerza, 9:Inteligencia).
+    Retorno:
+        None
+    """
+    print(f'Nombre: {heroe[0]}\n'
+          f'Identidad: {heroe[1]}\n'
+          f'Empresa: {heroe[2]}\n'
+          f'Altura: {heroe[3]} cm \n'
+          f'Peso: {heroe[4]} Kilos \n'
+          f'Género: {heroe[5]}\n'
+          f'Ojos: {heroe[6]}\n'
+          f'Pelo: {heroe[7]}\n'
+          f'Fuerza: {heroe[8]}\n'
+          f'Inteligencia: {heroe[9]}\n'
+          f'--------------------------'
+    )
+
+
+def ver_heroes(lista_heroe):
+    """
+    Brief:
+        Recorre la lista principal mediante un bucle for e imprime cada heroe usando sus indices.
+    Parametros:
+        lista_heroe: Lista bidimensional que contiene a todos los heroes guardados.
+    Retorno:
+        None
+    """
+    print('-- LISTA DE HEROES --')
+    for i in range(len(lista_heroe)):  
+        mostrar_heroe(lista_heroe[i]) 
